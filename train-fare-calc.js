33291f2b65d577a66951e12d672b155eade8b943
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-04-27 14:45:19 +0100

    updated variable names

--- train-fare-calc.js
+++ train-fare-calc.js
@@ -2,16 +2,18 @@ let age = prompt("What is your age?");
 let distance = prompt("How far are you travelling?");
 let price = 0.21
 let fare;
-let youngPersonsDiscount = 20;
-let olderPersonsDiscount = 40;
+let juniorDiscount = 20;
+let seniorDiscount = 40;
+
+
 if (age < 18) {
-    fare = (distance * price) * (1 - (youngPersonsDiscount/100));
-    alert(`Your train fare is ${fare}p`);
+    fare = (distance * price) * (1 - (juniorDiscount/100));
+    alert(`Your train fare is £${fare}`);
 } else if (age > 65) {
-    fare = (distance * price) * (1 - (olderPersonsDiscount/100));
-    alert(`Your train fare is ${fare}p`);
+    fare = (distance * price) * (1 - (seniorDiscount/100));
+    alert(`Your train fare is £${fare}`);
 } else {
     (fare = distance * price);
-    alert(`Your train fare is ${fare}p`);
+    alert(`Your train fare is £${fare}`);
 
 }
