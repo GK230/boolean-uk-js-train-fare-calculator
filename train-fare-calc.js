bb968920f34c189e3c0323c8ee5c0447a633cef3
Geeta Kotecha <geeta.kotecha@gmail.com>
2021-04-27 14:13:21 +0100

    completed basic exerise

---
+++ train-fare-calc.js
@@ -0,0 +1,13 @@
+let age = prompt("What is your age?");
+let distance = prompt("How far are you travelling?");
+let price = 0.21
+let fare;
+let youngPersonsDiscount = 20;
+let olderPersonsDiscount = 40;
+if (age < 18) {
+    fare = (distance * price) * (1 - (youngPersonsDiscount/100));
+} else if (age > 65) {
+    fare = (distance * price) * (1 - (olderPersonsDiscount/100));
+} else {
+    (fare = distance * price);
+}
